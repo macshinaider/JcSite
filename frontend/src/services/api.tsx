ff0a79fc49c0d2f1130be255856e3471156e1aff
MacShinaider <lucasadd15@gmail.com>
2023-06-20 14:08:57 -0300

    ✔

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:3000",
-	timeout: 5000,
+	baseURL: "http://localhost:8081",
+	timeout: 50000,
 	headers: {
 		"Content-Type": "application/json",
 	},
