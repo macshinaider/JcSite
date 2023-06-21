573131ff51dddff6c7f15f4fea6bc0b4e0b4a051
MacShinaider <lucasadd15@gmail.com>
2023-06-20 22:36:28 -0300

    arrumando erro

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const Footer = () => {
-  const [githubData, setGithubData] = useState<any>(null);
+	const [githubData, setGithubData] = useState<any>(null);
 
-  useEffect(() => {
-    fetch('https://api.github.com/users/macshinaider')
-      .then(response => response.json())
-      .then(data => setGithubData(data))
-      .catch(error => console.log(error));
-  }, []);
+	useEffect(() => {
+		fetch("https://api.github.com/users/macshinaider")
+			.then((response) => response.json())
+			.then((data) => setGithubData(data))
+			.catch((error) => console.log(error));
+	}, []);
 
-  return (
-    <footer className="bg-gray-200 py-4 absolute bottom-0 w-full">
-      <div className="container mx-auto text-center">
-        {githubData ? (
-            
-          <p>
-            <img className='w-24 h-24 rounded-full mx-auto' src={githubData.avatar_url} alt="avatar" />
-            Criador: <a href={githubData.html_url}>{githubData.login}</a>
-          </p>
-        ) : (
-          <p>Carregando...</p>
-        )}
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="bg-gray-200 py-4 absolute bottom-0 w-full">
+			<div className="container mx-auto text-center">
+				{githubData ? (
+					<p>
+						<img
+							className="w-24 h-24 rounded-full mx-auto"
+							src={githubData.avatar_url}
+							alt="avatar"
+						/>
+						Criador: <a href={githubData.html_url}>{githubData.login}</a>
+					</p>
+				) : (
+					<p>Carregando...</p>
+				)}
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
